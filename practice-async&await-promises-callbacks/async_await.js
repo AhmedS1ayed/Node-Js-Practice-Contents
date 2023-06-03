59b0c0de860d8d1e167718b45985d4119123eaa5
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-06-03 18:15:36 +0300

    async and await

---
+++ practice-async&await-promises-callbacks/async_await.js
@@ -0,0 +1,37 @@
+async function displayCommit() {
+  const user = await getuser(3);
+  const repos = await getRepository(user.gitHubUsername);
+  const commit = await getCommits(repos);
+  console.log(commit);
+}
+function getuser(id) {
+  console.log(`getting user with id = ${id} from database`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // some processes .....
+      resolve({ id: id, gitHubUsername: "Ahmed" });
+    }, 2000);
+  });
+}
+
+displayCommit();
+
+function getRepository(username) {
+  console.log(`getting repos of ${username} from database`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // some processes .....
+      resolve(["repo1", "repo2", "repo3"]);
+    }, 2000);
+  });
+}
+
+function getCommits(repos) {
+  console.log(`getting commit from database`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // some processes .....
+      resolve("#44fa");
+    }, 2000);
+  });
+}
