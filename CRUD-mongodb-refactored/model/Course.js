121b00bc069d361dc6be9b49a88234ee3f013056
AhmedS1ayed <eng.ahmed.sayed.abed@gmail.com>
2023-06-04 23:19:13 +0300

    adding custom validators

--- CRUD-mongodb-refactored/model/Course.js
+++ CRUD-mongodb-refactored/model/Course.js
@@ -3,12 +3,20 @@ const courseSchema = mongoose.Schema({
   _id: { type: String, default: () => Math.random().toString(36).slice(2) },
   name: String,
   author: { type: String, required: true },
-  category:{
-    type:String,
-    enum:['web','network'],
-    required:true
+  category: {
+    type: String,
+    enum: ["web", "network"],
+    required: true,
   },
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (v) {
+        return v && v.length > 0;
+      },
+      message: "A course should have at least 1 tag",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
   price: {
